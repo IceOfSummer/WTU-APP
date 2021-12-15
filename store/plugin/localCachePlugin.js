6b21ede73e2304bbed2d4f00cbb8321a9d580852
许德森 <2237803016@qq.com>
2021-12-15 23:19:50 +0800

    fix: 修复了vuex自动保存机制存在的潜在问题
    之前保存每个属性都是根据其属性名称作为唯一特征保存的，但不可避免的，后续开发可能会定义重复的属性名称，若state定义了多个名称相同的的属性，则这些属性的值不会被分开保存。
    
    因此在代码中, 使用
    JSON.stringify(Object.getOwnPropertyNames(target)) + key
    来作为唯一特征, 基本可以完全避免属性名重复的问题

--- store/plugin/localCachePlugin.js
+++ store/plugin/localCachePlugin.js
@@ -1,12 +1,16 @@
 export default function (store) {
 
+  /**
+   * 内部的flag是为了防止对象的key重复, 使用flag后除非两个对象完全相同, 否则不可能会出现重复key
+   */
   const localStorageHandler = {
     get(target, p) {
-      return target[p]
+      return target[p.toString()]
     },
     set(target, p, value) {
+      const flag = JSON.stringify(Object.getOwnPropertyNames(target))
       uni.setStorage({
-        key: p.toString(),
+        key: flag + p.toString(),
         data: value
       })
       target[p] = value
@@ -15,13 +19,14 @@ export default function (store) {
   }
 
   const loadStorage = (obj) => {
+    const flag = JSON.stringify(Object.getOwnPropertyNames(obj))
     Object.keys(obj).forEach(key => {
       if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
         // 另外一个对象, 继续代理
         obj[key] = loadStorage(obj[key])
       } else {
         // 数组或基本数据类型
-        obj[key] = uni.getStorageSync(key)
+        obj[key] = uni.getStorageSync(flag + key)
       }
     })
     return new Proxy(obj, localStorageHandler)
