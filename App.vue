63b78faf0facc328e6e71f08894c26aaa5b1d0c8
许德森 <2237803016@qq.com>
2022-01-05 22:12:39 +0800

    update: 将更新方式换为了静默更新

--- App.vue
+++ App.vue
@@ -1,33 +1,27 @@
 <script>
 import { getVersion } from './api/appVersion'
-import manifest from './manifest.json'
 import { showToast } from './hook/utils/TipUtils'
-import { formatSize } from './hook/utils/StringUtils'
 import { getServerUrl } from './hook/utils/serverUtils'
 export default {
+  data () {
+    return {
+      newVersionPath: ''
+    }
+  },
   onLaunch: function() {
     const serverUrl = getServerUrl()
+    const that = this
 
     // 检查更新
     getVersion().then(resp => {
-      const curVersion = manifest.versionCode
       const newVersion = resp.data.versionCode
-      const size = resp.data.size
-      const temp = newVersion - curVersion
-      console.log(`当前版本: ${curVersion}, 最新版本: ${newVersion}`)
-      if (temp >= 1) {
-        uni.showModal({
-          title: '发现新版本! 是否需要更新?',
-          content: `当前版本: ${curVersion}, 最新版本: ${newVersion}\n大小: ${formatSize(size)}`,
-          success ({ confirm }) {
-            if (confirm) {
-              // 确定更新
-              showToast('正在下载中, 下载完毕后会自动安装并重启')
-              downLoadNewVersion(resp.data.versionName)
-            }
-          }
-        })
-      }
+      plus.runtime.getProperty(plus.runtime.appid, (inf) => {
+        console.log(`当前版本: ${inf.versionCode}, 最新版本: ${newVersion}`)
+
+        if (newVersion - Number.parseInt(inf.versionCode) >= 1) {
+          downLoadNewVersion(resp.data.versionName)
+        }
+      })
     }).catch(e => console.log(e))
 
     /**
@@ -35,20 +29,13 @@ export default {
      * @param versionName {string}
      */
     function downLoadNewVersion (versionName) {
-      console.log(`${serverUrl}/vcs/app/hotUpdate/${versionName}`)
+      console.log('downloading: ' + versionName)
       uni.downloadFile({
         url: `${serverUrl}/vcs/app/hotUpdate/${versionName}`,
         success (result) {
           console.log(result)
           if (result.statusCode === 200) {
-            plus.runtime.install(result.tempFilePath, {
-              force: false
-            }, function() {
-              console.log('install success...')
-              plus.runtime.restart()
-            }, function() {
-              showToast('安装失败, 请稍后再试')
-            })
+            that.newVersionPath = result.tempFilePath
           }
         }
       })
@@ -59,7 +46,17 @@ export default {
     console.log('App Show')
   },
   onHide: function() {
-    console.log('App Hide')
+    // 隐藏APP后直接更新
+    if (this.newVersionPath) {
+      plus.runtime.install(this.newVersionPath, {
+        force: false
+      }, function() {
+        console.log('install success...')
+        plus.runtime.restart()
+      }, function() {
+        showToast('安装失败, 请稍后再试')
+      })
+    }
   }
 }
 </script>
